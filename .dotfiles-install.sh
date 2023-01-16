4b01cb62abb4200b9011760e4ecfe1c0974d1545
Jason Walker <jason.walker@onedatascan.com>
2023-01-16 14:05:11 -0500

    Added some simple symlinks

--- .dotfiles-install.sh
+++ .dotfiles-install.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
 echo "running dotfile install"
+
+for f in .gitconfig .ideavimrc .inputrc .vimrc .zshenv .zshrc; do
+  ln -s "${f}" "${HOME}"/"${f}"
+done
+
+# Work
+git config --global user.signingkey D39AF0B2
