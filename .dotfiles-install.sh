52277a37fb44f2f4769b20783d9ac4523d2ab3cf
Jason Walker <jason.walker@onedatascan.com>
2023-01-16 14:46:11 -0500

    Added bash stuff to install

--- .dotfiles-install.sh
+++ .dotfiles-install.sh
@@ -3,9 +3,9 @@
 # Assume this is run from the current directory
 dotfiles_dir="${PWD}"
 
-for f in .gitconfig .ideavimrc .inputrc .vimrc .zshenv .zshrc; do
+for f in .bash_profile .bashrc .gitconfig .ideavimrc .inputrc .vimrc .zshenv .zshrc; do
   (cd "${HOME}" && ln -s "${dotfiles_dir}"/"${f}" "${f}")
 done
 
-# Work
+# TODO this is hardcoded as work key. Figure out better way to deal with logic
 git config --global user.signingkey D39AF0B2
