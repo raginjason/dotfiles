586fcbdb8a4ddf292ff10f28b3d265671f18f5c8
Jason Walker <jason.walker@onedatascan.com>
2023-01-17 11:33:13 -0500

    Adjusted repo and logic

--- bootstrap-datascan.sh
+++ bootstrap-datascan.sh
@@ -4,8 +4,8 @@ machine_role='datascan'
 
 git config --global user.signingkey D39AF0B2
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
+if [ "$OSTYPE" == "darwin"* ]; then
   ./bootstrap-darwin.sh "${machine_role}"
-elif [[ "$OSTYPE" == "linux"* ]]; then
+elif [ "$OSTYPE" == "linux"* ]; then
   ./bootstrap-linux.sh "${machine_role}"
 fi
--- bootstrap-personal.sh
+++ bootstrap-personal.sh
@@ -4,8 +4,8 @@ machine_role='personal'
 
 git config --global user.signingkey 349DD25F
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
+if [ "$OSTYPE" == "darwin"* ]; then
   ./bootstrap-darwin.sh "${machine_role}"
-elif [[ "$OSTYPE" == "linux"* ]]; then
+elif [ "$OSTYPE" == "linux"* ]; then
   ./bootstrap-linux.sh "${machine_role}"
 fi
