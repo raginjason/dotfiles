837ed59aaab1044b54d0f5758f61502406ee7125
Jason Walker <jason.walker@fanduel.com>
2024-03-20 15:58:33 -0400

    Add FD config

---
+++ bootstrap-fanduel.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+dotfiles_home=`dirname $0`
+machine_role='fanduel'
+
+git config --global user.signingkey 1B17DF9CE0FF984A7BE2F033D7EF114F30003F59
+
+case $(uname | tr '[:upper:]' '[:lower:]') in
+  darwin*)
+    "${dotfiles_home}"/bootstrap-darwin.sh "${machine_role}"
+    /opt/homebrew/bin/brew install --cask rancher
+  ;;
+  linux*)
+    "${dotfiles_home}"/bootstrap-linux.sh "${machine_role}"
+  ;;
+esac
