6bc11fc08e078c121bd9f1b4a7481dd1e38b610c
Jason Walker <jason.walker@fanduel.com>
2024-06-12 15:33:44 -0400

    Add docker logic

--- bootstrap-linux.sh
+++ bootstrap-linux.sh
@@ -3,7 +3,17 @@
 machine_role="$1"
 dotfiles_home=$(realpath $(dirname $0))
 
-for f in .bash_profile .bash_prompt .bashrc .gitconfig .inputrc .vimrc .zshenv .zshrc .shell_variables ; do
+if [ -f /.dockerenv ]; then
+  declare -a files=(".bash_profile" ".bash_prompt" ".bashrc" ".inputrc" ".vimrc" ".zshenv" ".zshrc" ".shell_variables")
+else
+  declare -a files=(".bash_profile" ".bash_prompt" ".bashrc" ".gitconfig" ".inputrc" ".vimrc" ".zshenv" ".zshrc" ".shell_variables")
+  if command -v code >/dev/null; then
+    code --force --install-extension vscodevim.vim
+    code --force --install-extension streetsidesoftware.code-spell-checker
+  fi
+fi
+
+for f in "${files[@]}"; do
   if [ -e "${dotfiles_home}"/"${machine_role}"/"${f}" ]; then
     source_file="${dotfiles_home}"/"${machine_role}"/"${f}"
     echo "Using ${machine_role} override for ${f}"
@@ -17,7 +27,3 @@ for f in .bash_profile .bash_prompt .bashrc .gitconfig .inputrc .vimrc .zshenv .
   (cd "${HOME}" && ln -nsf "${source_file}" "${f}")
 done
 
-if command -v code >/dev/null; then
-  code --force --install-extension vscodevim.vim
-  code --force --install-extension streetsidesoftware.code-spell-checker
-fi
