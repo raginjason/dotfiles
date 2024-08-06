f986fae0318eeda1c3dc563d63497870c44c224f
Jason Walker <jason.walker@fanduel.com>
2024-08-06 15:34:30 -0400

    Move git config to darwin-only

--- bootstrap-fanduel.sh
+++ bootstrap-fanduel.sh
@@ -3,11 +3,10 @@
 dotfiles_home=`dirname $0`
 machine_role='fanduel'
 
-git config --global user.signingkey 1B17DF9CE0FF984A7BE2F033D7EF114F30003F59
-git config --global user.name "Jason Walker"
-
 case $(uname | tr '[:upper:]' '[:lower:]') in
   darwin*)
+    git config --global user.signingkey 1B17DF9CE0FF984A7BE2F033D7EF114F30003F59
+    git config --global user.name "Jason Walker"
     "${dotfiles_home}"/bootstrap-darwin.sh "${machine_role}"
     /opt/homebrew/bin/brew install --cask rancher
   ;;
