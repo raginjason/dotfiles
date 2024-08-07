5e11687449c5f4338d7e5207b06004a9e42bb9af
Jason Walker <ragin.jason@me.com>
2024-08-06 22:01:08 -0400

    Move git config to darwin-only for personal

--- bootstrap-personal.sh
+++ bootstrap-personal.sh
@@ -5,6 +5,8 @@ machine_role='personal'
 
 case $(uname | tr '[:upper:]' '[:lower:]') in
   darwin*)
+    git config --global user.signingkey 349DD25F
+    git config --global user.name "Jason Walker"
     "${dotfiles_home}"/bootstrap-darwin.sh "${machine_role}"
     /opt/homebrew/bin/brew install --cask docker
   ;;
@@ -13,7 +15,6 @@ case $(uname | tr '[:upper:]' '[:lower:]') in
   ;;
 esac
 
-git config --global user.signingkey 349DD25F
 
 if command -v dolt >/dev/null; then
   dolt config --global --add user.email "${GIT_AUTHOR_EMAIL}"
